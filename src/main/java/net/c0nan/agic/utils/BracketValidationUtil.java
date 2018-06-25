f26d3e570020ab0687dd5dfdd492333811183361
JannieP <jannie@pieterse.me>
2018-06-25 14:09:55 +1000

    replaced recursion with better queue LIFO algo

--- src/main/java/net/c0nan/agic/utils/BracketValidationUtil.java
+++ src/main/java/net/c0nan/agic/utils/BracketValidationUtil.java
@@ -2,82 +2,55 @@ package net.c0nan.agic.utils;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 @Service
 public class BracketValidationUtil {
 
     private static Map<Character, Character> bracketPairs = new HashMap<>();
+    private static List<Character> openBrackets = new ArrayList<>();
+    private static List<Character> closeBrackets = new ArrayList<>();
 
     static {
-        bracketPairs.put('(', ')');
-        bracketPairs.put('{', '}');
-        bracketPairs.put('[', ']');
+
+        bracketPairs.put(')', '(');
+        bracketPairs.put('}', '{');
+        bracketPairs.put(']', '[');
+
+        openBrackets.add('{');
+        openBrackets.add('[');
+        openBrackets.add('(');
+
+        closeBrackets.add('}');
+        closeBrackets.add(']');
+        closeBrackets.add(')');
+
     }
 
     public static boolean matchBrackets(final String value) {
         if (value.isEmpty()) {
             return true;
         }
-        boolean matching = true;
 
-        Map<Integer, Character> contextSortedBrackets = getContextSortedBrackets(value);
-
-        if (contextSortedBrackets.isEmpty()) {
-            matching = validateNoOrphanClosingBrackets(value);
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        for (Character charz : value.toCharArray()) {
+            if (openBrackets.contains(charz)) {
+                queue.add(charz);
+            }
+            if (closeBrackets.contains(charz)) {
+                if (queue.isEmpty() || queue.getLast() != bracketPairs.get(charz)) {
+                    return false;
+                }
+                queue.removeLast();
+            }
         }
 
-        for (Integer posOpen : contextSortedBrackets.keySet()) {
+        return queue.isEmpty();
 
-            Character openBracket = contextSortedBrackets.get(posOpen);
-
-            int posClosure = value.lastIndexOf(bracketPairs.get(openBracket));
-
-            if (posOpen < 0 && posClosure < 0) {
-                matching = true;
-            } else if (posOpen >= 0 && posOpen < posClosure) {
-                if (posOpen > 0) {
-                    matching = matchBrackets(value.substring(0, posOpen));
-                }
-                if (matching) {
-                    matching = matchBrackets(value.substring(posOpen + 1, posClosure));
-                }
-                if (matching && posClosure < value.length() - 1) {
-                    matching = matchBrackets(value.substring(posClosure + 1));
-                }
-                break;
-            } else {
-                matching = false;
-            }
-
-            if (!matching) {
-                break;
-            }
-
-        }
-        return matching;
     }
 
-    private static boolean validateNoOrphanClosingBrackets(final String value) {
-        for (Character bracket : bracketPairs.values()) {
-            if (value.indexOf(bracket) >= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static Map<Integer, Character> getContextSortedBrackets(final String value) {
-        Map<Integer, Character> contextSortedBrackets = new TreeMap<>();
-
-        for (Character openBracket : bracketPairs.keySet()) {
-            int posOpen = value.indexOf(openBracket);
-            if (posOpen >= 0) {
-                contextSortedBrackets.put(posOpen, openBracket);
-            }
-        }
-        return contextSortedBrackets;
-    }
 }
