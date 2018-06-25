660d96d480b6924d3de6f5b65791b738888909a2
JannieP <jannie@pieterse.me>
2018-06-25 13:12:07 +1000

    Added relaxedQueryChars = "[]"

--- src/main/java/net/c0nan/agic/TestAPIApplication.java
+++ src/main/java/net/c0nan/agic/TestAPIApplication.java
@@ -1,13 +1,32 @@
 package net.c0nan.agic;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"net.c0nan"})
 public class TestAPIApplication {
+
     public static void main(final String[] args) {
         SpringApplication.run(TestAPIApplication.class, args);
     }
+
+    @Bean
+    public TomcatServletWebServerFactory containerFactory() {
+        return new TomcatServletWebServerFactory() {
+            protected void customizeConnector(final Connector connector) {
+                super.customizeConnector(connector);
+                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+                    ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setRelaxedQueryChars("[]");
+                }
+            }
+        };
+
+    }
+
 }
